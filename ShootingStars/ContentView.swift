d0b78dae7e2cbf70b8784f655d2fabcab35769d6
Carlo Namoca <carlo.namoca@gmail.com>
2024-05-27 09:53:34 -0600

    Initial Commit

---
+++ ShootingStars/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ShootingStars
+//
+//  Created by Carlo Namoca on 2024-05-27.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ShootingStars/ShootingStarsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShootingStarsApp.swift
+//  ShootingStars
+//
+//  Created by Carlo Namoca on 2024-05-27.
+//
+
+import SwiftUI
+
+@main
+struct ShootingStarsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
